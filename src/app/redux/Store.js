a34d112493f15603ac1ac73936f29246eeefdc99
EliasRizon <ngothuan2422001@gmail.com>
2023-02-22 11:52:12 +0700

    update

--- src/app/redux/Store.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import 'regenerator-runtime/runtime'
-
-const sagaMiddleware = createSagaMiddleware()
